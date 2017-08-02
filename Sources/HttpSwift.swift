ac4d2e73aff98c8648b0250fafd8d05be87196b4
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2017-08-02 20:51:30 +0900

    fix

--- Sources/HttpSwift.swift
+++ Sources/HttpSwift.swift
@@ -11,78 +11,78 @@ public enum HttpMethod: String {
 }
 
 open class HTTP {
-    
+
     open let session: URLSession = {
         let conf = URLSessionConfiguration.default
         return URLSession(configuration: conf)
     }()
-    
+
     private var request = Request()
-    
+
     private init() {}
     public static let instance: HTTP = {
         return HTTP()
     }()
-    
+
     open func setHeader(headers: [String: String]) -> HTTP {
         self.request.headers = headers
         return self
     }
-    
+
     open func addHeader(key: String, value: String) -> HTTP {
         if self.request.headers == nil {
             self.request.headers = [String: String]()
         }
-        
+
         self.request.headers![key] = value
         return self
     }
-    
+
     open func setCookie(cookies: [String: String]) -> HTTP {
         self.request.cookies = cookies
         return self
     }
-    
+
     open func addCookie(key: String, value: String) -> HTTP {
         if self.request.cookies == nil {
             self.request.cookies = [String: String]()
         }
-        
+
         self.request.cookies![key] = value
         return self
     }
-    
+
     open func setContentsType(_ type: ContentsType) -> HTTP {
         return self.addHeader(key: "Content-Type", value: type.rawValue)
     }
-    
+
     open func setQuery(params: [String: String]) -> HTTP {
         self.request.params = params
         return self
     }
-    
+
     open func setUserAgent(agent: String) -> HTTP {
         return self.addHeader(key: "User-Agent", value: agent)
     }
-    
+
     open func setAuth(token: String) -> HTTP {
         return self.addHeader(key: "Authorization", value: "Beare " + token)
     }
-    
+
     open func basicAuth(id: String, pw: String) -> HTTP {
         guard let data = (id + ":" + pw).data(using: String.Encoding.utf8) else {
             return self
         }
-        
+
         let b64 = data.base64EncodedString()
         return self.addHeader(key: "Authorization", value: "Basic " + b64)
     }
-    
+
     private enum ProxyStatus: Int {
         case on  = 1
         case off = 0
     }
-    
+
     open func setProxy(host: String, port: String) -> HTTP {
         session.configuration.connectionProxyDictionary = [AnyHashable: Any]()
         session.configuration.connectionProxyDictionary?[kCFNetworkProxiesHTTPEnable as String] = ProxyStatus.on
@@ -90,84 +90,84 @@ open class HTTP {
         session.configuration.connectionProxyDictionary?[kCFNetworkProxiesHTTPPort   as String] = port
         return self
     }
-    
+
     open func delProxy() -> HTTP {
         session.configuration.connectionProxyDictionary?[kCFNetworkProxiesHTTPEnable as String] = ProxyStatus.off
         session.configuration.connectionProxyDictionary?[kCFNetworkProxiesHTTPProxy  as String] = ""
         session.configuration.connectionProxyDictionary?[kCFNetworkProxiesHTTPPort   as String] = ""
         return self
     }
-    
+
     open func get(url: String) -> HTTP {
         self.request.url = url
         self.request.method = HttpMethod.GET
         return self
     }
-    
+
     open class func Get(url: String) -> HTTP {
         return HTTP().get(url: url)
     }
-    
+
     open func post(url: String) -> HTTP {
         self.request.url = url
         self.request.method = HttpMethod.POST
         return self
     }
-    
+
     open class func Post(url: String) -> HTTP {
         return HTTP().post(url: url)
     }
-    
+
     open func put(url: String) -> HTTP {
         self.request.url = url
         self.request.method = HttpMethod.PUT
         return self
     }
-    
+
     open class func Put(url: String) -> HTTP {
         return HTTP().put(url: url)
     }
-    
+
     open func patch(url: String) -> HTTP {
         self.request.url = url
         self.request.method = HttpMethod.PATCH
         return self
     }
-    
+
     open class func Patch(url: String) -> HTTP {
         return HTTP().patch(url: url)
     }
-    
+
     open func delete(url: String) -> HTTP {
         self.request.url = url
         self.request.method = HttpMethod.DELETE
         return self
     }
-    
+
     open class func Delete(url: String) -> HTTP {
         return HTTP().delete(url: url)
     }
-    
+
     open func cancel() {
         session.invalidateAndCancel()
     }
-    
+
     //Execute Request
     open func `do`(handler: @escaping (Result<Any, HttpSwiftError>) -> ()) {
         guard let _ = request.url else {
             return
         }
-        
+
         session.dataTask(with: request.createRequest()) { data, response, error in
-            
+
             switch(data, response, error) {
             case(_, _, let error?):
                 handler(Result(error: .connectionError(error)))
             case(let data?, let resp, _):
-                
+
                 //Check Status Code
                 if 200..<300 ~= (resp as! HTTPURLResponse).statusCode {
-                    
+
                     //Check Contents Type
                     switch self.request.headers?["Content-Type"] {
                     case "json"?:
@@ -192,13 +192,13 @@ open class HTTP {
             }
         }.resume()
     }
-    
+
     //Execute Request
     open func `do`() {
         guard let _ = request.url else {
             return
         }
-        
+
         session.dataTask(with: request.createRequest()).resume()
     }
 }
