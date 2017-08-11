bf4168e7cd7350463cf5b9c5d9ccc4fd4415009f
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2017-08-11 13:00:20 +0900

    fix test code

--- Tests/HttpSwiftTests/HttpSwiftTests.swift
+++ Tests/HttpSwiftTests/HttpSwiftTests.swift
@@ -2,12 +2,14 @@ import XCTest
 @testable import HttpSwift
 
 class HttpSwiftTests: XCTestCase {
-    
+
     func testGetRequestWithoutHandler() {
         HTTP.instance.get(url: "http://httpbin.org/get").do()
     }
-    
+
     func testGetRequest() {
+        let expectation = self.expectation(description: "get request test")
+
         HTTP.instance
             .get(url: "http://httpbin.org/get").do { result in
             switch result {
@@ -16,10 +18,17 @@ class HttpSwiftTests: XCTestCase {
             case .failure(let error):
                 print(error)
             }
+
+            expectation.fulfill()
         }
+
+        waitForExpectations(timeout: 30, handler: nil)
     }
-    
+
     func testCustomHeader() {
+
+        let expectation = self.expectation(description: "custom header request test")
+
         HTTP.instance
             .get(url: "http://httpbin.org/headers")
             .setHeader(headers: ["key1": "1", "key2": "2"]).do { result in
@@ -29,10 +38,14 @@ class HttpSwiftTests: XCTestCase {
                 case .failure(let error):
                     print(error)
                 }
+            expectation.fulfill()
         }
+
+        waitForExpectations(timeout: 30, handler: nil)
     }
 
     static var allTests = [
         ("testGetRequest", testGetRequest),
+        ("testCustomHeader", testCustomHeader),
     ]
 }
